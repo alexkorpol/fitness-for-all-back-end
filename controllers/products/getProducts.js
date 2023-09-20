0bc033a625696539966378f5b857c92b423a2296
MovchanNatalia <Movchan.n2106@Gmail.com>
2023-09-20 23:09:03 +0300

    add filter products

---
+++ controllers/products/getProducts.js
@@ -0,0 +1,36 @@
+const { Product } = require("../../models");
+
+const getProducts = async (req, res) => {
+  const { title, category, recommended } = req.body;
+
+  const { blood } = req.user.bodyData;
+
+  const query = {};
+
+  if (recommended === "true") {
+    const field = `groupBloodNotAllowed.${blood}`;
+    query[field] = true;
+  }
+
+  if (recommended === "false") {
+    const field = `groupBloodNotAllowed.${blood}`;
+    query[field] = false;
+  }
+
+  if (category) {
+    query.category = category;
+  }
+
+  if (title) {
+    const titleRegex = new RegExp(title, "i");
+    query.title = titleRegex;
+  }
+
+  const allProducts = await Product.find(query);
+
+  res.status(200).json({
+    allProducts,
+  });
+};
+
+module.exports = getProducts;
--- controllers/products/index.js
+++ controllers/products/index.js
@@ -1,5 +1,7 @@
 const getCategory = require('./getCategory');
+const getProducts = require("./getProducts");
 
 module.exports = {
   getCategory,
+  getProducts
 };
