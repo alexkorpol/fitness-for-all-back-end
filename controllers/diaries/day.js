84b2ffdf1cbfbf84b8ef5419cd6674ae068c2ea7
LISOVSMA <116366290+LISOVSMA@users.noreply.github.com>
2023-09-27 21:40:30 +0300

    corr swagger

--- controllers/diaries/day.js
+++ controllers/diaries/day.js
@@ -1,28 +1,28 @@
-const { Day } = require('../../models');
-const moment = require('moment');
-const { HttpError, ctrlWrapper } = require('../../helpers');
+const { Day } = require("../../models");
+const moment = require("moment");
+const { HttpError, ctrlWrapper } = require("../../helpers");
 
 const getDay = async (req, res) => {
-    const { date } = req.params;
-    const { _id: owner } = req.user;
+  const { date } = req.params;
+  const { _id: owner } = req.user;
 
-    const queryDate = date || moment().format('DD-MM-YYYY');
-    console.log(queryDate);
-    const data = await Day.findOne({ date: queryDate, owner })
-        .populate({
-            path: 'products.productId',
-            model: 'product',
-        })
-        .populate({
-            path: 'exercises.exerciseId',
-            model: 'exercise',
-        })
-        .select('-createdAt -updatedAt');
-    if (!data) {
-        throw HttpError(404, 'Day not found');
-    }
+  const queryDate = date || moment().format("DD-MM-YYYY");
 
-    res.json(data);
+  const data = await Day.findOne({ date: queryDate, owner })
+    .populate({
+      path: "products.productId",
+      model: "product",
+    })
+    .populate({
+      path: "exercises.exerciseId",
+      model: "exercise",
+    })
+    .select("-createdAt -updatedAt");
+  if (!data) {
+    throw HttpError(404, "Day not found");
+  }
+
+  res.json(data);
 };
 
 module.exports = { getDay: ctrlWrapper(getDay) };
